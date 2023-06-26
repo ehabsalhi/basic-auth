b10b2dfa70aa8eaae3b7c49c4e5f99b0c0625421
ehabsalhi <ehab1salhe@gmail.com>
2023-06-26 11:39:57 +0000

    bearer auth 1

--- src/moduls/index.js
+++ src/moduls/index.js
@@ -1,4 +1,7 @@
 const { Sequelize, DataTypes } = require("sequelize");
+const bcrypt = require('bcrypt')
+const jwt = require ('jsonwebtoken')
+const secretKey = process.env.SECRET
 
 require('dotenv').config()
 
@@ -15,8 +18,36 @@ const User = sequelize.define('userlab' , {
      password : {
           type : DataTypes.STRING,
           allowNull : false
+     },
+     token :{
+          type : DataTypes.VIRTUAL
      }
 })
+
+
+User.basicChecker = async function (username, password)  {
+
+     const checkUser = await User.findOne({where :{username}})
+     const isValid = await  bcrypt.compare(password , checkUser.password)
+     if(isValid){
+
+          const parsToken = jwt.sign({username : checkUser.username , password : checkUser.password} ,secretKey )
+          return{
+               userInfo : checkUser,
+              Token : parsToken
+          }
+     }
+}
+
+User.beareCheacker =async function (token){
+     const verfyToken = jwt.verify(token , secretKey)
+     const user = await User.findOne({where :{username : verfyToken.username}})
+     if(user.username){
+          return user
+     }
+     throw new Error(' no token ') 
+}
+
 
 
 module.exports = {
