31b1301842ac918ce8c16d8f57ad5dcd4e69b948
ehabsalhi <ehab1salhe@gmail.com>
2023-07-03 06:51:23 +0000

    dev - 7.1

--- src/middleware/signin.js
+++ src/middleware/signin.js
@@ -6,11 +6,7 @@ module.exports = async(req , res , next) => {
      const auth = req.headers.authorization.split(" ").pop()
      const [username , password] = base64.decode(auth).split(':')
 
-     User.basicChecker(username , password).then(data => {
-          console.log(data);
-          req.data = data
-          next()
-     }
+
      const isValid = await bcrybt.compare(password , checkUser?.password)
      console.log(isValid);
      req.valid = isValid
